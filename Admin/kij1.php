c4b97ee7b0b999795def96a6cfdddf41a3906e62
Tensbeem <tensrrt1234@gmail.com>
2022-01-25 12:31:58 +0700

    add

---
+++ Admin/kij1.php
@@ -0,0 +1,20 @@
+<?php include ("includes/header.php"); ?>
+<?php include ("includes/sidebar.php"); ?>
+<?php include ("includes/topbar.php"); ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">ภาพกิจกรรม 1</h1>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            
+
+<?php include("includes/footer.php"); ?>
+<?php include("includes/logputmodal.php"); ?>
+<?php include("includes/scripts.php"); ?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -8,31 +8,23 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link active" aria-current="page" href="index.php">หน้าแรก</a>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Menu
+            เกี่ยวกับเรา
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">ข้อมูลส่วนตัว</a></li>
             <li><a class="dropdown-item" href="#">ระบบจัดการ</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Log out</a></li>
+            <li><a class="dropdown-item" href="#">ออกจากระบบ</a></li>
           </ul>
         </li>
         
         <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="register.php">Register</a>
+          <a class="nav-link" href="user/login.php">Blog</a>
         </li>
 
       </ul>
